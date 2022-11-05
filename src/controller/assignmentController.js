a426c9bd33b7365fc824c5bd6afcb04074696d58
Hussayn <hussaynabdsamad07@gmail.com>
2022-11-05 22:44:55 +0100

    fixes

--- src/controller/assignmentController.js
+++ src/controller/assignmentController.js
@@ -13,6 +13,37 @@ class AssignmentController {
 
   async operation(req, res) {
     try {
+      const validations = [
+        check("operation_type")
+          .notEmpty()
+          .bail()
+          .withMessage("operation_type is required")
+          .isString()
+          .withMessage("operation_type must be a string")
+          .matches(/(add|multiply|subtract|x|\+|\-|times|minus)/)
+          .withMessage("operation_type mismatched"),
+        check("x")
+          .notEmpty()
+          .withMessage("x field is required")
+          .isInt()
+          .withMessage("x must be a number"),
+        check("y")
+          .notEmpty()
+          .withMessage("y field is required")
+          .isInt()
+          .withMessage("y must be a number"),
+      ];
+
+      for (let validation of validations) {
+        const result = await validation.run(req);
+        if (result.errors.length) break;
+      }
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+      }
+
       let result = null;
       let x = Number(req.body.x);
       let y = Number(req.body.y);
@@ -22,7 +53,7 @@ class AssignmentController {
      */
       let operation_type = null;
       let operationType = req.body.operation_type.toLowerCase();
-    
+
       if (operationType.includes("add") || operationType.includes("+")) {
         operation_type = "addition";
         result = x + y;
@@ -31,7 +62,7 @@ class AssignmentController {
         operation_type = "subtraction";
         result = x - y;
       }
-    
+
       if (operationType.includes("mul") || operationType.includes("*")) {
         operation_type = "multiplication";
         result = x * y;
@@ -40,7 +71,7 @@ class AssignmentController {
         operation_type = "addition";
         result = x + y;
       }
-    
+
       if (operationType.includes("pro")) {
         operation_type = "multiplication";
         result = x * y;
@@ -49,7 +80,7 @@ class AssignmentController {
         operation_type = "multiplication";
         result = x * y;
       }
-    
+
       if (operationType.includes("diff")) {
         operation_type = "subtraction";
         result = x - y;
@@ -58,7 +89,7 @@ class AssignmentController {
       return res.status(200).json({
         slackUserName: "hussaynabdsamad07",
         result,
-        operation_type,
+        operation_type: operation_type,
       });
     } catch (error) {
       return res.status(400).json({
