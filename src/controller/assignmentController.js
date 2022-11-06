d01e5f92101d017cdb09c9f1e69a40558af24820
Hussayn <hussaynabdsamad07@gmail.com>
2022-11-06 16:39:35 +0100

    fixes

--- src/controller/assignmentController.js
+++ src/controller/assignmentController.js
@@ -13,71 +13,40 @@ class AssignmentController {
 
   async operation(req, res) {
     try {
-      // const validations = [
-      //   check("operation_type")
-      //     .notEmpty()
-      //     .bail()
-      //     .withMessage("operation_type is required")
-      //     .isString()
-      //     .withMessage("operation_type must be a string")
-      //     .matches(/(add|multiply|subtract|x|\+|\-|times|minus)/)
-      //     .withMessage(
-      //       'operation_type mismatched'
-      //     ),
-      //   check("x")
-      //     .notEmpty()
-      //     .withMessage("x field is required")
-      //     .isInt()
-      //     .withMessage("x must be a number"),
-      //   check("y")
-      //     .notEmpty()
-      //     .withMessage("y field is required")
-      //     .isInt()
-      //     .withMessage("y must be a number"),
-      // ];
-
-      // for (let validation of validations) {
-      //   const result = await validation.run(req);
-      //   if (result.errors.length) break;
-      // }
-      // const errors = validationResult(req);
-
-      // if (!errors.isEmpty()) {
-      //  return res.status(400).json({ errors: errors.array() });
-      // }
-
+      let result = null;
+      let x = Number(req.body.x);
+      let y = Number(req.body.y);
       let operation_type = null;
+      if(typeof req.body.operation_type !== "string") throw new Error("Operation type must be a string");
       let operationType = req.body.operation_type.toLowerCase();
-    
-      if (operationType.includes("add") || operationType.includes("+")) {
+
+      console.log(req.body);
+      if (isNaN(x) || isNaN(y)) {
+        throw new Error("Input must be a number");
+      }
+      /** 
+        @type {string} operationType
+        @type {Number}  x
+        @type {Number} y
+          */
+
+      let matchAdd = /(add|addition|\+|plus)/;
+      let matchSubtraction = /(deduct|subtract|\-|minus)/;
+      let matchMultiply = /(multiplication|multiply|x|\*|times)/;
+
+      if (matchAdd.test(operationType)) {
+        result += x + y;
         operation_type = "addition";
-        result = x + y;
       }
-      if (operationType.includes("sub") || operationType.includes("-")) {
+      if (matchSubtraction.test(operationType)) {
+        result += x - y;
         operation_type = "subtraction";
-        result = x - y;
       }
-      if (operationType.includes("pro")) {
+      if (matchMultiply.test(operationType)) {
+        result += x * y;
         operation_type = "multiplication";
-        result = x * y;
       }
-      if (operationType.includes("times")) {
-        operation_type = "multiplication";
-        result = x * y;
-      }
-      if (operationType.includes("mul") || operationType.includes("*")) {
-        operation_type = "multiplication";
-        result = x * y;
-      }
-      if (operationType.includes("sum")) {
-        operation_type = "addition";
-        result = x + y;
-      }
-    
-      if (operationType.includes("diff")) {
-        operation_type = "subtraction";
-        result = x - y;
-      }
+
       // submitted late because of school test forgive me
       return res.status(200).json({
         slackUserName: "hussaynabdsamad07",
