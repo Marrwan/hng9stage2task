0a94beea1aca077995ff15b312ce6d754b64cab0
Hussayn <hussaynabdsamad07@gmail.com>
2022-11-06 16:10:46 +0100

    fixes

--- src/controller/assignmentController.js
+++ src/controller/assignmentController.js
@@ -13,70 +13,70 @@ class AssignmentController {
 
   async operation(req, res) {
     try {
-      const validations = [
-        check("operation_type")
-          .notEmpty()
-          .bail()
-          .withMessage("operation_type is required")
-          .isString()
-          .withMessage("operation_type must be a string")
-          .matches(/(add|multiply|subtract|x|\+|\-|times|minus)/)
-          .withMessage(
-            'operation_type mismatched'
-          ),
-        check("x")
-          .notEmpty()
-          .withMessage("x field is required")
-          .isInt()
-          .withMessage("x must be a number"),
-        check("y")
-          .notEmpty()
-          .withMessage("y field is required")
-          .isInt()
-          .withMessage("y must be a number"),
-      ];
+      // const validations = [
+      //   check("operation_type")
+      //     .notEmpty()
+      //     .bail()
+      //     .withMessage("operation_type is required")
+      //     .isString()
+      //     .withMessage("operation_type must be a string")
+      //     .matches(/(add|multiply|subtract|x|\+|\-|times|minus)/)
+      //     .withMessage(
+      //       'operation_type mismatched'
+      //     ),
+      //   check("x")
+      //     .notEmpty()
+      //     .withMessage("x field is required")
+      //     .isInt()
+      //     .withMessage("x must be a number"),
+      //   check("y")
+      //     .notEmpty()
+      //     .withMessage("y field is required")
+      //     .isInt()
+      //     .withMessage("y must be a number"),
+      // ];
 
-      for (let validation of validations) {
-        const result = await validation.run(req);
-        if (result.errors.length) break;
-      }
-      const errors = validationResult(req);
+      // for (let validation of validations) {
+      //   const result = await validation.run(req);
+      //   if (result.errors.length) break;
+      // }
+      // const errors = validationResult(req);
 
-      if (!errors.isEmpty()) {
-       return res.status(400).json({ errors: errors.array() });
-      }
+      // if (!errors.isEmpty()) {
+      //  return res.status(400).json({ errors: errors.array() });
+      // }
 
-      let result = 0;
-      let x = Number(req.body.x);
-      let y = Number(req.body.y);
-      let operation_type = "";
-      let operationType = req.body.operation_type;
-
-      if (
-        operationType.includes("add") ||
-        operationType.includes("addition") ||
-        operationType.includes("+")
-      ) {
-        result += x + y;
+      let operation_type = null;
+      let operationType = req.body.operation_type.toLowerCase();
+    
+      if (operationType.includes("add") || operationType.includes("+")) {
         operation_type = "addition";
+        result = x + y;
       }
-      if (
-        operationType.includes("multiply") ||
-        operationType.includes("x") ||
-        operationType.includes("*") ||
-        operationType.includes("multiplication") ||
-        operationType.includes("times")
-      ) {
-        result += x * y;
-        operation_type = "multiplication";
-      }
-      if (
-        operationType.includes("subtraction") ||
-        operationType.includes("minus") ||
-        operationType.includes("-")
-      ) {
-        result += x - y;
+      if (operationType.includes("sub") || operationType.includes("-")) {
         operation_type = "subtraction";
+        result = x - y;
+      }
+      if (operationType.includes("pro")) {
+        operation_type = "multiplication";
+        result = x * y;
+      }
+      if (operationType.includes("times")) {
+        operation_type = "multiplication";
+        result = x * y;
+      }
+      if (operationType.includes("mul") || operationType.includes("*")) {
+        operation_type = "multiplication";
+        result = x * y;
+      }
+      if (operationType.includes("sum")) {
+        operation_type = "addition";
+        result = x + y;
+      }
+    
+      if (operationType.includes("diff")) {
+        operation_type = "subtraction";
+        result = x - y;
       }
       // submitted late because of school test forgive me
       return res.status(200).json({
